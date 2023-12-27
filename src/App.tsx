5dfd46f33d8baeaa0772c46207a845028286b049
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-27 15:50:17 -0800

    Configured redux store, actions, reducers, and implemented sample fetch from json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
 import "./App.css";
 import stacklineLogo from "./assets/stackline_logo.svg";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchProductsFailure,
+  fetchProductsSuccess,
+  fetchProductsRequest,
+} from "./redux/actions/productActions";
+
+const STACKLINE_SAMPLE_PRODUCTS_DATA_PATH =
+  "/stackline_frontend_assessment_data_2021.json";
 
 function App() {
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state);
+
+  // Fetch data from the test API
+  useEffect(() => {
+    console.log("fetching data");
+    const fetchData = async () => {
+      dispatch(fetchProductsRequest());
+      try {
+        const response = await fetch(STACKLINE_SAMPLE_PRODUCTS_DATA_PATH);
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        const data = await response.json();
+        console.log(data);
+        dispatch(fetchProductsSuccess(data));
+      } catch (error) {
+        console.log(error);
+        dispatch(fetchProductsFailure((error as Error).message));
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  // Check the store
+  console.log("store", store);
+
   return (
     <div className="flex flex-col w-screen h-screen">
       <header className="flex h-20 w-full bg-[#052849] shadow-lg">
