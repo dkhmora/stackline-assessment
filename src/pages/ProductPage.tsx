1902fec8bd768d2482a92ac8e6abdfbf8e85965d
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-29 14:18:16 -0800

    Improved mobile reactivity to Header and ProductPage

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -9,8 +9,8 @@ interface ProductPageProps {
 
 export default function ProductPage({ product }: ProductPageProps) {
   return (
-    <div className="flex space-x-5 px-5 py-16 animate-fadeIn ease-in-out duration-100">
-      <section className="flex flex-grow w-1/5">
+    <div className="flex flex-col px-2 py-8 md:flex-row md:space-x-5 space-y-10 md:space-y-0 md:px-5 md:py-16 animate-fadeIn ease-in-out duration-100">
+      <section className="flex w-full md:flex-grow md:w-2/5 lg:w-1/5">
         <ProductDetailsCard
           image={product.image}
           title={product.title}
@@ -19,7 +19,7 @@ export default function ProductPage({ product }: ProductPageProps) {
         />
       </section>
 
-      <section className="flex flex-col w-4/5 space-y-16">
+      <section className="flex flex-col w-full space-y-10 md:w-3/5 lg:w-4/5 md:space-y-16">
         <SalesChartCard sales={product.sales} />
 
         <SalesTableCard sales={product.sales} />
