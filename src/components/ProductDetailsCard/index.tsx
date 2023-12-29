1076c31d7fa19a965220896035f00f812d392672
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-28 23:56:23 -0800

    Made some font sizes smaller in ProductDetailsCard

--- src/components/ProductDetailsCard/index.tsx
+++ src/components/ProductDetailsCard/index.tsx
@@ -15,7 +15,7 @@ export default function ProductDetailsCard({
 }: ProductDetailsCardProps) {
   return (
     <div className="flex flex-1 flex-col items-center shadow-md bg-white rounded overflow-hidden">
-      <div className="flex flex-col items-center p-6 border-b">
+      <div className="flex flex-col items-center p-6">
         <img
           src={image}
           alt={`${title}-product-image`}
@@ -24,13 +24,13 @@ export default function ProductDetailsCard({
 
         <div className="px-6 mt-5">
           <h1 className="text-2xl font-bold py-2 text-gray-700">{title}</h1>
-          <h2 className="text-lg font-light tracking-wide text-gray-400">
+          <h2 className="text-md font-light tracking-wide text-gray-400">
             {subtitle}
           </h2>
         </div>
       </div>
 
-      <div className="flex flex-wrap w-full px-6 py-3 justify-between border-b">
+      <div className="flex flex-wrap w-full px-6 py-3 border-y">
         {tags
           ? tags.map((tagText, index) => (
               <Tag text={tagText} key={`${tagText}${index}`} />
